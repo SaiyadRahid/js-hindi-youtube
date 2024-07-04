052cd3b106a6bb0c5af72fffb300b3dd3aa5346d
SaiyadRahid <33921625+SaiyadRahid@users.noreply.github.com>
2024-07-04 11:20:33 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = '155543';
+let accountEmail = 'rahid@google.com';
+var accountPass = '1234';
+accountCity = 'Pune';
+
+accountEmail = 'rahid@ibm.com';
+ccountPass = '2222';
+accountCity = 'Bangaluru';
+
+//accountId = 2;
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity]);

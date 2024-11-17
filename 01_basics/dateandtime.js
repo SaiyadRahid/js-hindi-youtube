bf2f9a0aaf4fb03d1460d4e8823247641304d343
SaiyadRahid <33921625+SaiyadRahid@users.noreply.github.com>
2024-11-17 07:41:08 +0000

    datatime explain

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,17 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+// let myCreatedDate = new Date("2023-01-14");
+// let myCreatedDate = new Date("11-16-2024");
+let myCreatedDate = new Date("2023-01-14");
+// console.log(myCreatedDate.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
